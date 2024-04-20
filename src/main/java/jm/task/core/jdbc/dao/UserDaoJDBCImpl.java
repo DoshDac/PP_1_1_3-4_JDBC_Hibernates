5f44753faaad1460d8fb9a38e04563fa5bd926f9
Ali <x1q8qkijnf@protonmail.com>
2024-04-20 05:28:56 +0300

    выполнено задание

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +14,81 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS USERS (" +
+                    "id BIGINT NOT NULL AUTO_INCREMENT," +
+                    "name VARCHAR(64)," +
+                    "lastName VARCHAR(64)," +
+                    "age SMALLINT," +
+                    "PRIMARY KEY ( id ) )"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()){
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES(?, ?, ?)")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement prepStat = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            prepStat.setLong(1, id);
+            prepStat.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
 
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new LinkedList<>();
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
+
+
 
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static Connection connection;
+    public static Connection getConnection() {
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "ali", "root");
+        } catch (SQLException e) {
+            System.out.println("Пал бу, нет подключения");
+        }
+        return connection;
+    }
 }
